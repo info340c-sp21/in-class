f976e8b037b9f043116bd549bf018e2372d8f3fc
Jacob A. Morris <trauma@uw.edu>
2021-03-30 19:19:24 -0700

    day-02 starter

---
+++ day-02/day02.spec.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const cheerio = require('cheerio') //for html testing
+
+//include custom matchers
+const styleMatchers = require('jest-style-matchers');
+expect.extend(styleMatchers);
+
+const htmlPath = __dirname + '/index.html';
+const html = fs.readFileSync(htmlPath, 'utf-8'); //load the HTML file once
+
+describe('Source code is valid', () => {
+  test('HTML validates without errors', async () => {
+    const lintOpts = {
+      'attr-bans':['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], //adding height, allow longdesc
+      'doctype-first':true,
+      'doctype-html5':true,
+      'html-req-lang':true,
+      'attr-name-style': false, //for meta tags
+      'line-end-style':false, //either way
+      'indent-style':false, //can mix/match
+      'indent-width':false, //don't need to beautify
+      'class-style':'none', //I like dashes in classnames
+      'img-req-alt':false, //for this test; captured later!
+    }
+
+    await expect(htmlPath).toHaveNoHtmlLintErrorsAsync(lintOpts);
+  })  
+});
