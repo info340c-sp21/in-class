da53f1facab75833e2fadcad93146ee55e995aca
Jacob A. Morris <trauma@uw.edu>
2021-04-19 17:52:27 -0700

    added day 10

---
+++ day-10/day-10/README.md
@@ -0,0 +1,5 @@
+# jQuery
+
+Practice creating and manipulating elements using jQuery. The final page will look like this:
+
+![page after being manipulated by jquery](imgs/complete.png)
---
+++ day-10/day-10/js/index.js
@@ -0,0 +1,34 @@
+// Main JavaScript File
+'use strict';
+
+let state = {
+    text: 'Here is some text.'
+};
+// Wait for you page to load to assign events to the elements created in your index.html File
+$(function () {
+    // Select all svgs and change their attr "height" to 200
+
+
+    // Select your first svg and change it's width attribute to 10
+
+
+    // Select the input and set the attribute "placeholder" to "start typing"
+
+
+    // Set the input val to state.text
+
+
+    // Select elements with class "italics" and set their css "font-style", to "italic"
+
+
+    // Select even rows in the table and set their css "background" to "lightgray"
+
+
+    // Assign an event handler to the <input> element that updates state.text
+
+    // Create a new paragraph, add some text, and add it as the *first* element in the <main> tag
+
+    // Assign an click event to the <button> element that changes the text of 
+    // element with id "change" to the state.text, and resets the input
+
+});
